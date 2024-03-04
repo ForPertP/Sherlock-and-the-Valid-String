97c9645f4ec68e915d81577d3678bac409c5948c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-03-05 03:24:55 +0900

    Update Sherlock-and-the-Valid-String.cs

--- Sherlock-and-the-Valid-String.cs
+++ Sherlock-and-the-Valid-String.cs
@@ -1,1 +1,30 @@
-cs
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.isValid(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
