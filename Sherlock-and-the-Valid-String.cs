17b120fc37172454fedd83d85bef6633a5b039a5
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-03-07 05:03:19 +0900

    Update Sherlock-and-the-Valid-String.cs

--- Sherlock-and-the-Valid-String.cs
+++ Sherlock-and-the-Valid-String.cs
@@ -24,6 +24,40 @@ class Result
 
     private static string IsValid_ByMap(string s)
     {
+        if (s.Length == 1) return "YES";
+
+        Dictionary<char, int> charCounts = new Dictionary<char, int>();
+        foreach (char c in s)
+        {
+            if (charCounts.ContainsKey(c))
+                charCounts[c]++;
+            else
+                charCounts[c] = 1;
+        }
+
+        int minCount = int.MaxValue;
+        int maxCount = int.MinValue;
+        foreach (var entry in charCounts)
+        {
+            minCount = Math.Min(minCount, entry.Value);
+            maxCount = Math.Max(maxCount, entry.Value);
+        }
+
+        if (minCount == maxCount) return "YES";
+
+        int minCountFreq = 0;
+        int maxCountFreq = 0;
+        foreach (var entry in charCounts)
+        {
+            if (entry.Value == minCount) minCountFreq++;
+            if (entry.Value == maxCount) maxCountFreq++;
+        }
+
+        if ((maxCount - minCount == 1 && maxCountFreq == 1) ||
+            (minCount == 1 && minCountFreq == 1 && maxCountFreq == charCounts.Count - 1))
+            return "YES";
+
+        return "NO";
     }
 
     private static string IsValid_ByVector(string s)
